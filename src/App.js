5922734c7050bd46c61f94be5b595105ec7f0480
marioalbornoz <mario.albornozc@utem.cl>
2020-07-27 17:43:02 -0400

    contenedor agregado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
 
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+  max-width:600px;
+  margin: 0 auto;
+`;  
+
+const ContenedorFormulario = styled.div`
+  background-color:#fff;
+  padding:3rem;
+`;  
+
 function App() {
   return (
-      <Header
-        titulo = "Cotizador de Seguros"
-      />
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
